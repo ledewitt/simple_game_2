9eade0fccff198c0d802198df8ef45212f0f786a
Luke DeWitt <ledewitt@gmail.com>
2012-03-26 17:09:11 -0600

    Setting up lib.

---
+++ lib/simple_game.rb
@@ -0,0 +1,3 @@
+inside simple_game.rb
+require_relative "simple_game/player.rb"
+...
---
+++ lib/simple_game/location.rb
@@ -0,0 +1,20 @@
+class Location # Set up what I need for a location
+  attr_accessor :name, :description, :objects, :exits  # better as reader
+  attr_reader :exit_descriptions
+  
+  def initialize(name, description, objects, exits)
+    @name              = name
+    @description       = description
+    @objects           = objects.split(/,\s*/)
+    @exit_descriptions = exits.split(/,\s*/)
+    @exits             = { }
+  end
+
+  def link_exits(locations)
+    @exit_descriptions.each do |exit_description|
+      direction         = exit_description[/\A[A-Z]+/]
+      location_name     = exit_description[/[A-Z ]+\Z/].strip
+      @exits[direction] = locations.find { |l| l.name == location_name }
+    end
+  end
+end
