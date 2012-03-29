630ca5aa6657b6063c75ab5b1099a3b7f70d5daa
Luke DeWitt <ledewitt@gmail.com>
2012-03-29 10:33:58 -0600

    Working the problem.

--- lib/simple_game.rb
+++ lib/simple_game.rb
@@ -1,9 +1,4 @@
 # inside simple_game.rb
+require_relative "simple_game/game.rb"
 require_relative "simple_game/player.rb"
 require_relative "simple_game/location.rb"
-require_relative "simple_game/game.rb"
-
-# module SimpleGame
-#   class Player
-#   end
-# end
